9b0eb39accf8df4da0581c69f8e2fc6ca9f45891
Sagar Saha <Sagarsaha2579@gmail.com>
2025-07-05 19:07:43 -0400

    initial commit for Variables-Exercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             string MyName = "Sagar"; 
+             int MyAge = 27;
+             char MyGender = 'M';
+             bool IsMale = true;
+             double MyWeight = 176.5;
+             decimal CAD = 0.74m;  
+             
+             Console.WriteLine($"Name: {MyName}"); 
+             Console.WriteLine($"Gender: I am a Male or{MyGender} for short");
+        }
+    }
+}
